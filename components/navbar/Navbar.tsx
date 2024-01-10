4f75c5a771f4cfdc8568cee82b11d37e6687a919
Jose Manuel Munoz Gutierrez <manuelinux@gmail.com>
2024-01-09 21:52:33 -0400

    First commit

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import { HomeIcon } from '@primer/octicons-react';
+import { ActiveLink } from '..';
+
+const navItems = [
+    { path: '/about', text:'About' },
+    { path: '/pricing', text:'Pricing' },
+    { path: '/contact', text:'Contact' },
+]
+
+export const NavBar = () => {
+  return (
+    <nav className='flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded'>
+        <Link className='flex items-center' href='/'>
+            <HomeIcon className='mr-2'/>
+            <span>Home</span>
+        </Link>
+        <div className='flex flex-1'></div>
+        {
+            navItems.map((navItem, index) => (
+                <ActiveLink key={index} {...navItem} />
+            ))
+        }
+    </nav>
+  )
+}
